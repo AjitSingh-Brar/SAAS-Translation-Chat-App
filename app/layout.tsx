6c63a4a3f72677c4f1edc67ec3b0eb70880c7302
NS918326 <Navrajgaltee@outlook.com>
2024-10-12 17:48:15 -0400

    ADD: Landing page implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ClientProviders from "@/components/ClientProviders";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "SAAS-Translation-Chat-App",
@@ -30,7 +30,7 @@ export default function RootLayout({
     <ClientProviders>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className="flex flex-col min-h-screen"
         >
           <ThemeProvider
             attribute="class"
